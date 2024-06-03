6c7e5f02e4829cd904e82addf06e16a51f6ec7bc
karasumeyra <sumeyrakara93@gmail.com>
2024-06-03 13:29:16 +0200

    Failed Test Runner2

--- src/test/java/com/eurotech/runners/FailedTestRunner.java
+++ src/test/java/com/eurotech/runners/FailedTestRunner.java
@@ -7,14 +7,22 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/failed-html-reports.html"},
-        features = "@target/rerun.txt",
+        features = "@target/rerun.txt", // hatali testlerin kosmasi icin
         glue = "com/eurotech/step_definitions"
 )
 
 public class FailedTestRunner { // eski versiyonlarda bu class olusturuluyordu,simdi zorunlu degil
     // bazen eski versiyonlar kullanilabilir, o zaman bu class olusturulmak zorunda
-    // yeni versiynlar icin olusturulmak zorunda degil
-
+    // yeni versiyonlar icin olusturulmak zorunda degil.
 
 
 }
+
+/*
+maven, bir gelistirme sistemidir
+kendi icinde bir yapisi vardir. bize dependences'leri yani kutuphaneleri almamizi saglar.
+Ayrica kendi life cycle ile projemizi kurabiliriz.maven ile tüm ayarlari yapariz.
+maven'in olmazsa olmaz'i,beyni POM file'dir. butun proje burada kurulur ve yurutulur.
+
+
+ */
